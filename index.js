e103122f815f78bde7be8cfeb542bd550e97318a
coopertc <14ctc2@queensu.ca>
2019-05-09 14:42:57 -0400

    add default route

--- index.js
+++ index.js
@@ -4,31 +4,19 @@ const app = express()
 const db = require('./queries')
 const port = 3000
 
-console.log(process.env.DATABASE_URL);
-
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
-/*
+
 app.get('/', async (request, response) => {
-  console.log('hello')
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT * FROM users");
-    const results = { 'results': (result) ? result.rows : null };
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
+  response.json({ info: 'The beginings of the amplifire backend API' });
   }
 });
-*/
 
-app.get('/', db.getUsers)
+app.get('/users', db.getUsers)
 /*
 app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
