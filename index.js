d450df242093a680335818028250ef578e5d0def
coopertc <14ctc2@queensu.ca>
2019-05-09 13:18:09 -0400

    End connection after GET

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.use(
   })
 )
 
+const { Client } = require('pg');
+
+const client = new Client
+({
+  connectionString: 'postgres://dbdheidwdrcexm:9871bd5ad3c6bf47e729009968a4e7c80f4c124118b88d680d95e2e6c59a9f2b@ec2-174-129-208-118.compute-1.amazonaws.com:5432/d7fcl8rbk5cc0n',
+  ssl: true,
+});
+
+client.connect();
 
 
 
@@ -20,22 +29,12 @@ app.use(
 
 app.get('/', (request, response) => {
   console.log('hello')
-  const { Client } = require('pg');
-
-  const client = new Client
-  ({
-    connectionString: 'postgres://dbdheidwdrcexm:9871bd5ad3c6bf47e729009968a4e7c80f4c124118b88d680d95e2e6c59a9f2b@ec2-174-129-208-118.compute-1.amazonaws.com:5432/d7fcl8rbk5cc0n',
-    ssl: true,
-  });
-
-  client.connect();
   client.query('SELECT * FROM users;', (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
       response.json({info: JSON.stringify(row)});
     }
   });
-  client.end();
 });
 
 
