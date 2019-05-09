5a2859eb0bdd85e59ee4ca4a4f462146460e983b
coopertc <14ctc2@queensu.ca>
2019-05-09 13:02:58 -0400

    Changed query

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/', (request, response) => {
 });
 
 
-app.get('/users', db.getUsers)
+//app.get('/users', db.getUsers)
 /*
 app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
